8ddb9d417db6c6d0439b94fd79ae7081c2a189b8
Alberto Nanni <76218487+alberto-nanni@users.noreply.github.com>
2021-11-01 18:28:49 +0100

    Update hack.js

--- src/hack.js
+++ src/hack.js
@@ -9,6 +9,8 @@ const hash = '5e7d28e2cfff93edefb2d15abad07ec5';
 // When you figure it out, create a Pull Request on github with value. 
 // First correct PR wins a Lifetime PRO  membership and T-shirt
 
+// paste the hash on https://www.md5online.org/md5-decrypt.html
+
 ///// ANSWER /////
 
 const hacked = 'superhacker';
