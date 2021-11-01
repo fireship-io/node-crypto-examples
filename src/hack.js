42b3bb7842802807a1c4de782b1e34ad332d62f0
Sundararaman <vsundar17697@outlook.com>
2021-11-01 23:42:39 +0530

    solved

--- src/hack.js
+++ src/hack.js
@@ -5,11 +5,30 @@
 
 const hash = '5e7d28e2cfff93edefb2d15abad07ec5';
 
+const { assert } = require('console');
+var ReverseMd5 = require('reverse-md5')
+
+function hacker(hash) {
+
+    const rev = ReverseMd5({
+        lettersUpper: true,
+        lettersLower: true,
+        numbers: true,
+        special: true,
+        whitespace: true,
+        maxLen: 15
+    })
+    console.log('Here')
+    const obj = rev(hash);
+    return obj.str;
+}
+
 
 // When you figure it out, create a Pull Request on github with value. 
 // First correct PR wins a Lifetime PRO  membership and T-shirt
 
 ///// ANSWER /////
-
 const hacked = 'superhacker';
+
+assert(hacker(hash) === hacked, 'You did not hack the hash!');
 
